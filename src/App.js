97af8c17891c69f5dae65a835d939bd3ff77d3dc
rkiang <ryanlkiang@gmail.com>
2018-03-01 16:14:50 -0600

    added search vaule from input

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+    }
+    this.handlesSearchClick = this.handlesSearchClick.bind(this);
+  }
+
+  handleInputChange (event) {
+    this.setState({
+      searchText: event.target.value,
+    })
+  }
 
   handlesSearchClick () {
     console.log('button was clicked');
-    
+    console.log('search value was ' + this.state.searchText);
   }
 
   render() {
     return (
       <div>
-        <input placeholder="Search for a movie title" />
+        <input placeholder="Search for a movie title" value={this.state.searchText} onChange={event => this.handleInputChange(event)}/>
         <button onClick={this.handlesSearchClick}>Search</button>
       </div>
     );
